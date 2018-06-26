eb7c87c0d9b034235a40fc7cba97bd44f2e96fb4
Kalai Vendhan <kvcenation5@gmail.com>
2018-06-27 02:44:28 +0530

    .

---
+++ apostrophe.py
@@ -0,0 +1,8 @@
+message="Python strength is it diverse community "
+print(message)
+
+message1="Python's strength is in its diverse community"
+print(message1)
+
+message='Pythons strength is its diverse community'
+print(message)
---
+++ name_spaces.py
@@ -0,0 +1,7 @@
+name="Saro"
+print("Hello "+ name + ''', when are "you" starting to learn python ''')
+
+name="Kalai"
+print(name.upper())
+print(name.lower())
+print(name.title())
